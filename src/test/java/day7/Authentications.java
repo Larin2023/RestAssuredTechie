ae638dde01ce3d23a4d5ebcdab7d0db8c3ba1b2a
Alex Kolesnyk <alexander.v.anderson@gmail.com>
2023-10-18 20:53:50 -0400

    Completed Projects Techie & Smarot

--- src/test/java/day7/Authentications.java
+++ src/test/java/day7/Authentications.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 
 import  io.restassured.http.ContentType;
-import io.restassured.module.jsv.JsonSchemaValidator;
+//import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
